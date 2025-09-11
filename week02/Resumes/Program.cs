febe5f180c48d07ad910d9ffb49f34ed67461a61
schoolonlyaccount <macman5@tkf7.net>
2025-09-11 15:44:07 -0500

    Completed the Resumes learning assignment

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,30 @@
 using System;
+using System.Runtime.InteropServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Developer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+        job1.Display();
+
+        Job job2 = new Job();
+        job2._jobTitle = "Software Developer";
+        job2._company = "Microsoft";
+        job2._startYear = 2019;
+        job2._endYear = 2022;
+        job2.Display();
+
+        Resume myResume = new Resume();
+        myResume._name = "Kell Brine";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
