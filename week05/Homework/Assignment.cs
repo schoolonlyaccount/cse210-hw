be83260858b05d21ad3ceaaef1ecb38d69157616
schoolonlyaccount <macman5@tkf7.net>
2025-09-30 15:57:15 -0500

    Added Homework Activity Program

---
+++ week05/Homework/Assignment.cs
@@ -0,0 +1,14 @@
+public class Assignment
+{
+    private string _studentName;
+    private string _topic;
+
+    public Assignment(string studentName, string topic)
+    {
+        _studentName = studentName;
+        _topic = topic;
+    }
+
+    public string GetSummary() => $"{_studentName} - {_topic}";
+    public string GetStudentName() => _studentName;
+}
--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        void LineBreaker() => Console.WriteLine("<>");
+
+        Console.Clear();
+
+        Assignment a1 = new Assignment("Kell Brine", "Multiplication");
+        Console.WriteLine(a1.GetSummary());
+
+        LineBreaker();
+
+        MathAssignment a2 = new MathAssignment("Kell Brine", "Fractions", "7.3", "8-19");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        LineBreaker();
+
+        WritingAssignment a3 = new WritingAssignment("Kell Brine", "European History", "The Causes of World War II");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
